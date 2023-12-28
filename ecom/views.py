fd326f59b35d5718dfb4e6297ee3ff2e1b3f1ed3
kiran_sn <kiran@sopeonow.com>
2023-12-28 20:40:25 +0530

    Test urls->views->templates

--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+##K Custom views
+def index(request):
+    return render(request, 'ecom\index.html')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+##K Custom definitions
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = 'media/'
 
